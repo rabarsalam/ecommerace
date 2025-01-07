d5b086c24bb693080db04437e983189157eafbc3
rabarkg <133387844+rabarkg@users.noreply.github.com>
2025-01-07 21:18:31 +0300

    Install Package

--- app/page.tsx
+++ app/page.tsx
@@ -93,7 +93,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Go to aa.org →
         </a>
       </footer>
     </div>
