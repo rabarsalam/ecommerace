431841e0578bc05042cdd8566049d21c329d0c5b
rabarkg <133387844+rabarkg@users.noreply.github.com>
2025-01-13 23:59:29 +0300

    Last Update For Banner Section

--- app/components/ImageSwipper/SwiperSlideImage.tsx
+++ app/components/ImageSwipper/SwiperSlideImage.tsx
@@ -1,37 +1,92 @@
-// Import Swiper React components
+"use client";
+import Image from 'next/image';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
-
+import 'swiper/css/navigation';
 import './styles.css';
-
-// import required modules
-import { Pagination } from 'swiper/modules';
+// import Image
+import Iphone15 from '@/public/Images/Iphone.svg'
+import AppleLogo from '@/public/Images/AppleLogo.svg'
+import { Parallax, Pagination } from 'swiper/modules';
+import Link from 'next/link';
 
 export default function SwiperSlideImage() {
-    return (
-      <>
-        <Swiper
-          spaceBetween={30}
-          pagination={{
-            clickable: true,
-          }}
-          modules={[Pagination]}
-          className="mySwiper"
-        >
-          <SwiperSlide>Slide 1</SwiperSlide>
-          <SwiperSlide>Slide 2</SwiperSlide>
-          <SwiperSlide>Slide 3</SwiperSlide>
-          <SwiperSlide>Slide 4</SwiperSlide>
-          <SwiperSlide>Slide 5</SwiperSlide>
-          <SwiperSlide>Slide 6</SwiperSlide>
-          <SwiperSlide>Slide 7</SwiperSlide>
-          <SwiperSlide>Slide 8</SwiperSlide>
-          <SwiperSlide>Slide 9</SwiperSlide>
-        </Swiper>
-      </>
-    );
-  }
-  
+  return (
+    <>
+      <div className=' relative right-96 top-0 ' style={{width:'1000px',height:'400px'}}>
+          <Swiper
+          slidesPerView={1}
+          spaceBetween={50}
+            loop={true}
+            speed={600}
+            parallax={true}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Parallax, Pagination]}
+            className="mySwiper "
+          >
+            <SwiperSlide >
+              <div className='grid gap-10'>
+                <div className="title flex items-center gap-4 " data-swiper-parallax="-300">
+                <Image src={AppleLogo} width={40} height={40} alt='AppleLogo' /> 
+                <h1 className=' text-base font-light'>Iphone 14 Series</h1>
+                </div>
+                <div className="subtitle " data-swiper-parallax="-200">
+                  <h1 className='text-7xl'>
+                  Up to 20% <br/> off Voucher
+                  </h1>
+                </div>
+                <div className="text" data-swiper-parallax="-100">
+                  <Link href='/' >
+                  <h1 className='underline underline-offset-8 text-lg'> Shop Now</h1>
+                  </Link>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide >
+              <div className='grid gap-10'>
+                <div className="title flex items-center gap-4 " data-swiper-parallax="-300">
+                <Image src={AppleLogo} width={40} height={40} alt='AppleLogo' /> 
+                <h1 className=' text-base font-light'>Iphone 15 Series</h1>
+                </div>
+                <div className="subtitle " data-swiper-parallax="-200">
+                  <h1 className='text-7xl'>
+                  Up to 10% <br/> off Voucher
+                  </h1>
+                </div>
+                <div className="text" data-swiper-parallax="-100">
+                  <Link href='/' >
+                  <h1 className='underline underline-offset-8 text-lg'> Shop Now</h1>
+                  </Link>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide >
+            <div className='grid gap-10'>
+                <div className="title flex items-center gap-4 " data-swiper-parallax="-300">
+                <Image src={AppleLogo} width={40} height={40} alt='AppleLogo' /> 
+                <h1 className=' text-base font-light'>Iphone 16 Series</h1>
+                </div>
+                <div className="subtitle " data-swiper-parallax="-200">
+                  <h1 className='text-7xl'>
+                  Up to 5% <br/> off Voucher
+                  </h1>
+                </div>
+                <div className="text" data-swiper-parallax="-100">
+                  <Link href='/' >
+                  <h1 className='underline underline-offset-8 text-lg'> Shop Now</h1>
+                  </Link>
+                </div>
+              </div>
+            </SwiperSlide>
+            <div >
+              <Image className='absolute top-12 right-10' src={Iphone15} width={550}  alt='Iphone'/>
+            </div>
+          </Swiper>
+      </div>
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <>
       {/* First Section */}
-      <div className="grid grid-cols-2 items-center justify-around">
-        <div>
+      <div className="grid grid-cols-2 items-center justify-center px-20 ">
+        <div className="border-r-2 w-1/4">
           <ul>
             <div className="grid grid-cols-1 gap-3 items-center  ">
               {DataForMenuBar.map((list) => {
@@ -23,7 +23,7 @@ export default function Home() {
             </div>
           </ul>
         </div>
-        <div>
+        <div className="mr-10 mt-10 w-3/4 ">
           <SwiperSlideImage />
         </div>
       </div>
