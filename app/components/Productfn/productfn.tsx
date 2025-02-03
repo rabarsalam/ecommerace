8407b82fc3b4c906a6611df031a6099bdbea76b8
rabarkg <133387844+rabarkg@users.noreply.github.com>
2025-02-03 13:22:41 +0300

    Add Api Product

--- app/components/Productfn/productfn.tsx
+++ app/components/Productfn/productfn.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Image from "next/image";
+import Link from "next/link";
 
 // Define the Product type
 interface Product {
@@ -42,34 +43,38 @@ export default function Productfn() {
   return (
     <div className="flex flex-col items-center justify-between mt-20 gap-4">
       <h1 className="text-6xl font-bold">New Arrivals</h1>
-      <div className="grid grid-cols-4 gap-6 mx-10">
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-6 mx-10">
         {products.length > 0 ? (
           products.slice(0,8).map((product) => (
             <div
               key={product.id}
               className="flex flex-col text-center items-center justify-between gap-1 p-4  rounded-xl "
             >
-              <div className="shadow-md bg-DGray w-72 h-64 p-4 rounded-3xl text-center flex items-center justify-center">
+                
+              <div className="shadow-md bg-DGray w-36 h-36 md:w-72 md:h-64 p-12 md:p-4 rounded-3xl text-center flex items-center justify-center">
                 <Image src={product.image} width={100} height={100} alt={product.title} />
               </div>
-              <h1 className="text-xl font-bold">{product.title}</h1>
-              <div className="flex items-center justify-between gap-3">
+              <h1 className="md:text-xl font-bold">
+                {product.title.split(" ").slice(0, 6).join(" ") + (product.title.split(" ").length > 5 ? "..." : "")}
+              </h1>
+
+              <div className="flex items-center justify-between gap-1 text-xs md:text-base md:gap-3">
                 <h1>{renderStars(product.rating.rate)}</h1>
                 <h1>{product.rating.rate}/5</h1>
               </div>
               <div className="flex items-center justify-between gap-3">
-                <h1 className="font-bold">${product.price}</h1>
-                <h1 className="line-through text-TGray font-bold">
+                <h1 className="text-sm md:text-base  font-bold">${product.price}</h1>
+                <h1 className="text-sm md:text-base line-through text-TGray font-bold">
                   ${(product.price * 1.2).toFixed(2)}
                 </h1>
-                <h1 className="bg-red-100 rounded-full px-4 py-2 text-red-500">
+                <h1 className="bg-red-100 rounded-full md:px-4 md:py-2 text-red-500 text-xs md:text-base">
                   -20%
                 </h1>
               </div>
             </div>
           ))
         ) : (
-          <h1 className="text-2xl font-bold">Loading...</h1>
+          <h1 className="md:text-2xl font-bold">Please Waiting...</h1>
         )}
       </div>
     </div>
